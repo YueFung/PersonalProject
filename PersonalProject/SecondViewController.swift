165f0fdef7dc62fed8d8a0429c5056ba431068ec
Yue Fung Lee <yuefunglee@yues-macbook-pro.local>
2018-12-26 18:44:46 +0800

    Initial Commit

---
+++ PersonalProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PersonalProject
+//
+//  Created by Yue Fung Lee on 26/12/2018.
+//  Copyright © 2018 Yue Fung Lee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
