12afe5ca30e0ac8533e0e254969bf10779449eab
Yue Fung Lee <yuefunglee@yues-macbook-pro.local>
2018-12-27 22:32:46 +0800

    initial

--- PersonalProject/FirstViewController.swift
+++ PersonalProject/FirstViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import SwiftyGif
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
 
+    var slides:[Slide] = [];
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        scrollView.delegate = self
+        
+    func createSlides() -> [Slide] {
+        
+        let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
+        slide1.imageView.image = UIImage(named: "ic_onboarding_1")
+        slide1.labelTitle.text = "A real-life bear"
+        slide1.labelCreds.text = "Did you know that Winnie the chubby little cubby was based on a real, young bear in London"
+        
+    
+    
+        func setupSlideScrollView(slides : [Slide]) {
+            scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+            scrollView.isPagingEnabled = true
+            
+            for i in 0 ..< slides.count {
+                slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+                scrollView.addSubview(slides[i])
+        
+
+        
+            
+        //let gif = UIImage(gifName: "breathing1.gif")
+        //let imageView = UIImageView(gifImage: gif, loopCount: -1)
+        //self.image1.setGifImage(gif)
+        
+        
     }
 
 
 }
 
+        return [slide1]
+}
+}
+}
---
+++ PersonalProject/Slide.swift
@@ -0,0 +1,19 @@
+//
+//  Slide.swift
+//  
+//
+//  Created by Kelly Shu on 26/12/2018.
+//
+
+import UIKit
+
+
+class Slide: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelTitle2: UILabel!
+    @IBOutlet weak var labelCreds: UILabel!
+
+
+}
