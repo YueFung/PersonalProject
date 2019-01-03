989941e88633165416a44c7b8dacdee949110cb5
Kelly S <kxllys@gmail.com>
2019-01-03 22:15:13 +0800

    help omg this SUCKS

--- PersonalProject/FirstViewController.swift
+++ PersonalProject/FirstViewController.swift
@@ -7,25 +7,35 @@
 //
 
 import UIKit
-import SwiftyGif
 
 class FirstViewController: UIViewController, UIScrollViewDelegate {
     
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var scrollView: UIScrollView!{
+        didSet{
+            scrollView.delegate = self
+        }
+    }
+    
     @IBOutlet weak var pageControl: UIPageControl!
 
+    
     var slides:[Slide] = [];
     
-    
-    func setupSlideScrollView(slides : [Slide]) {
-        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
-        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
-        scrollView.isPagingEnabled = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
         
-        for i in 0 ..< slides.count {
-            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
-            scrollView.addSubview(slides[i])
-            
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        view.bringSubviewToFront(pageControl)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
             func createSlides() -> [Slide] {
                 
                 let slide1:Slide = Bundle.main.loadNibNamed("Slide", owner: self, options: nil)?.first as! Slide
@@ -69,37 +79,68 @@ class FirstViewController: UIViewController, UIScrollViewDelegate {
                 slide7.labelTitle.text = "How Breathing Helps"
                 slide7.labelTitle2.text = "Deep breathing helps relax tension you didn't realise you were holding (such as clenching your jaw or tightening your shoulders)"
                 slide7.labelCreds.text = "gif from destressmonday.org"
-                
-                return [slide1, slide2, slide3, slide4, slide5, slide6, slide7]
-                
-                
-                
-                //let gif = UIImage(gifName: "breathing1.gif")
-                //let imageView = UIImageView(gifImage: gif, loopCount: -1)
-                //self.image1.setGifImage(gif)
-                
-                
-            }
+        
+                  return [slide1, slide2, slide3, slide4, slide5, slide6, slide7]
+    
+    }
+            
+    func setupSlideScrollView(slides : [Slide]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
             
         }
         
     }
-
-    override func viewDidLoad() {
     
-        super.viewDidLoad()
-        slides = createSlides()
-        setupSlideScrollView(slides: [Slide])
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
         
-        pageControl.numberOfPages = slides.count
-        pageControl.currentPage = 0
-        view.bringSubviewToFront(pageControl)
+        let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
+        let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
+        
+        // vertical
+        let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+        let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
+        
+        let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
+        let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
         
      
-        scrollView.delegate = self
+        let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
+        
+        if(percentOffset.x > 0 && percentOffset.x <= 0.25) {
+            
+            slides[0].imageView.transform = CGAffineTransform(scaleX: (0.25-percentOffset.x)/0.25, y: (0.25-percentOffset.x)/0.25)
+            slides[1].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.25, y: percentOffset.x/0.25)
+            
+        } else if(percentOffset.x > 0.25 && percentOffset.x <= 0.50) {
+            slides[1].imageView.transform = CGAffineTransform(scaleX: (0.50-percentOffset.x)/0.25, y: (0.50-percentOffset.x)/0.25)
+            slides[2].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.50, y: percentOffset.x/0.50)
+            
+        } else if(percentOffset.x > 0.50 && percentOffset.x <= 0.75) {
+            slides[2].imageView.transform = CGAffineTransform(scaleX: (0.75-percentOffset.x)/0.25, y: (0.75-percentOffset.x)/0.25)
+            slides[3].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.75, y: percentOffset.x/0.75)
+            
+        } else if(percentOffset.x > 0.75 && percentOffset.x <= 1) {
+            slides[3].imageView.transform = CGAffineTransform(scaleX: (1-percentOffset.x)/0.25, y: (1-percentOffset.x)/0.25)
+            slides[4].imageView.transform = CGAffineTransform(scaleX: percentOffset.x, y: percentOffset.x)
+        }
+                
+                
+            }
+         
+
+
         
 
 
 
-}
+
+
 }
